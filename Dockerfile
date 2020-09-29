9f11affe0034858ded1efe0e2f7ef20212ed78b5
ube <ube@alienautopsy.net>
2020-09-29 09:55:38 +0200

    Revert "Python2"

--- Dockerfile
+++ Dockerfile
@@ -8,22 +8,21 @@ RUN yum -y update && yum -y install epel-release && yum -y install https://repo.
     git2u-core \
     make \
     nodejs \
-    python-setuptools \
-    python-virtualenv && \
+    python36-setuptools \
+    python36-virtualenv \
+    rsync && \
     mkdir /app
 
 ENV venvname ""
-ENV venvpath "/opt/venvs"
+ENV venvpath "/opt/venvs/"
 ENV packages ""
 
 VOLUME /out
 
 COPY scripts/make-venv.sh /app
-RUN chmod +x /app/make-venv.sh && \
-    ln -s /app/make-venv.sh /app/mktar.sh
-
+RUN chmod +x /app/make-venv.sh
 
 WORKDIR "/app/"
 
 ENTRYPOINT ["/bin/bash", "-c"]
-CMD ["./make-venv.sh"]
+CMD ["/app/make-venv.sh"]
--- scripts/make-venv.sh
+++ scripts/make-venv.sh
@@ -3,18 +3,17 @@
 function usage () {
     echo "Usage:"
     echo "docker run -it --rm -e packages=\"<list of pip packages>\" -e venvname=<name of venv> [-e venvpath=<path to \$venevname] -v <tarfile save dir>:/out venevbuilder [tar]"
-    echo "venvpath defaults to \"/opt/venvs\""
-    echo "To build tarfile directly:"
-    echo "docker run -it --rm -e packages=\"<list of pip packages>\" -e venvname=<name of venv> [-e venvpath=<path to \$venevname] -v <tarfile save dir>:/out venevbuilder ./mktar.sh"
+    echo "venvpath defaults to \"/opt/venvs/\""
+    echo "argument 'tar' to create tarfile instead of just copying."
     exit 1
 }
 
-export PATH=/app/:$PATH
 
 if [ "$packages" == "" ] || [ -z $venvname ] 
     then
     usage
 fi
+
 venv=$( echo ${venvpath}/$venvname/ | sed -r 's#/+#/#g')
 
 if [ -f $venv ]
@@ -22,28 +21,17 @@ if [ -f $venv ]
     echo "Removing old venv @ $venv"
     rm -rf $venv
 fi
+
+
 echo "Building $venv"
 mkdir -p $venv
-#python -m virtualenv --python=/usr/bin/python3 $venv
-virtualenv $venv
-#$venv/bin/pip install --upgrade pip
+python3 -m virtualenv --python=/usr/bin/python3 $venv
 $venv/bin/pip install $packages
-if [ "$0" == "./mktar.sh" ]
+if [ "$1" = "tar" ]
 then
-    tarfile=$(echo $venv | cut -b2- | sed 's#/#-#g' | sed -r 's#(.*)-#\1#g').tar
-    if [ -f /out/$tarfile ]
-    then
-        echo "$tarfile found in out directory. Delete."
-        rm /out/$tarfile
-    fi
-    echo "Taring.. $(echo $venv | cut -b2- | sed 's#/#-#g' | sed -r 's#(.*)-#\1#g').tar"
-    tar cf /out/$(echo $venv | cut -b2- | sed 's#/#-#g' | sed -r 's#(.*)-#\1#g').tar $venv
+echo "Taring.. $(echo $venv | cut -b2- | sed 's#/#-#g' | sed -r 's#(.*)-#\1#g').tar"
+tar cf /out/$(echo $venv | cut -b2- | sed 's#/#-#g' | sed -r 's#(.*)-#\1#g').tar $venv
 else
-    if [ -d /out/$venv ]
-    then
-        echo "/out/$venv found in out directory. Delete."
-        rm -rf /out/$venv
-    fi
-    echo "Copying venv."
-    cp -r $venv /out/
+echo "Copying venv."
+cp -r $venv /out/
 fi
